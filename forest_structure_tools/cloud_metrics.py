c281d67f0a8af81a13fe5f6e473c02b51403d9a1
jameswesc <jms.wsc@gmail.com>
2025-08-11 13:42:15 +1000

    wip

--- forest_structure_tools/cloud_metrics.py
+++ forest_structure_tools/cloud_metrics.py
@@ -46,7 +46,9 @@ def percentile_metrics(z: np.ndarray, percentiles=np.arange(10, 100, 10)):
 
 
 @with_suffix
-def cover_metrics(z: np.ndarray, weights: np.ndarray | None = None, cutoffs=[2]):
+def percent_at(
+    z: np.ndarray, threshold: float | list[float] = 2, weights: np.ndarray | None = None
+):
     if weights is None:
         zw = np.ones(z.shape)
     else:
@@ -54,16 +56,101 @@ def cover_metrics(z: np.ndarray, weights: np.ndarray | None = None, cutoffs=[2])
 
     total = zw.sum()
 
-    cover_metrics = {}
+    if not isinstance(threshold, list):
+        threshold = [threshold]
 
-    # If ground poitns are included calculate proportion ground
-    if z.min() == 0:
-        cover_metrics["ground_%"] = zw[z == 0].sum() / total * 100
+    metrics = {}
 
-    for cutoff in cutoffs:
-        cover_metrics[f"above_{cutoff}m_%"] = zw[z > cutoff].sum() / total * 100
+    for t in threshold:
+        metrics[f"%at_{t}m"] = zw[z == t].sum() / total * 100
 
-    return cover_metrics
+    return metrics
+
+
+@with_suffix
+def percent_above(
+    z: np.ndarray,
+    threshold: float | list[float] = 2,
+    weights: np.ndarray | None = None,
+    inclusive=False,
+):
+    if weights is None:
+        zw = np.ones(z.shape)
+    else:
+        zw = weights
+
+    total = zw.sum()
+
+    if not isinstance(threshold, list):
+        threshold = [threshold]
+
+    metrics = {}
+
+    for t in threshold:
+        label = f"gt{"e" if inclusive else ""}"
+        mask = z >= t if inclusive else z > t
+        metrics[f"%{label}_{t}m"] = zw[mask].sum() / total * 100
+
+    return metrics
+
+
+@with_suffix
+def percent_below(
+    z: np.ndarray,
+    threshold: float | list[float] = 2,
+    weights: np.ndarray | None = None,
+    inclusive=False,
+):
+    if weights is None:
+        zw = np.ones(z.shape)
+    else:
+        zw = weights
+
+    total = zw.sum()
+
+    if not isinstance(threshold, list):
+        threshold = [threshold]
+
+    metrics = {}
+
+    for t in threshold:
+        label = f"lt{"e" if inclusive else ""}"
+        mask = z <= t if inclusive else z < t
+        metrics[f"%{label}_{t}m"] = zw[mask].sum() / total * 100
+
+    return metrics
+
+
+@with_suffix
+def percent_in(
+    z: np.ndarray,
+    threshold: tuple[float, float] | list[tuple[float, float]] = (0, 1),
+    weights: np.ndarray | None = None,
+    inclusive=(False, True),
+):
+    if weights is None:
+        zw = np.ones(z.shape)
+    else:
+        zw = weights
+
+    total = zw.sum()
+
+    if not isinstance(threshold, list):
+        threshold = [threshold]
+
+    metrics = {}
+
+    for lt, ut in threshold:
+
+        llab = "[" if inclusive[0] else "("
+        ulab = "]" if inclusive[1] else ")"
+
+        lmask = z >= lt if inclusive[0] else z > lt
+        umask = z <= ut if inclusive[1] else z < ut
+
+        metrics[f"%in_{llab}{lt},{ut}m{ulab}"] = zw[lmask & umask].sum() / total * 100
+
+    return metrics
 
 
 @with_suffix
@@ -82,10 +169,10 @@ def relative_height_profile_metrics(
     (densities, _) = np.histogram(z, bins=bins, weights=weights, density=True)
 
     bounds = np.linspace(0, 100, bins + 1).astype(int)
-    labels = [f"{lower}-{upper}%" for lower, upper in zip(bounds, bounds[1:])]
+    labels = [f"[{lower},{upper}%)" for lower, upper in zip(bounds, bounds[1:])]
+    labels[-1] = labels[-1].replace("%)", "%]")
 
     for label, count, density in zip(labels, counts, densities):
-        metrics[f"prp_{label}"] = count / total
-        metrics[f"dns_{label}"] = density
+        metrics[f"%in_{label}"] = count / total * 100
 
     return metrics
