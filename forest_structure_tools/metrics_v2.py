808cdcc8c04bacf26d6f0c5af56faea5a1fb4527
jameswesc <jms.wsc@gmail.com>
2025-09-07 15:45:30 +1000

    wip

--- forest_structure_tools/metrics_v2.py
+++ forest_structure_tools/metrics_v2.py
@@ -3,61 +3,76 @@ import numpy.typing as npt
 
 import xarray as xr
 
-from scipy.stats import kurtosis, skew, entropy
+from scipy.stats import kurtosis, skew
 from .gini import gini
 from .cv import cv
 
-default_percentiles = np.arange(10, 100, 10)
 
-
-def forest_structure_metrics(
-    points: npt.NDArray,
-    xy_bin_size: float | None = 1,
-    z_bin_size: float = 1,
-    percentiles: npt.NDArray[np.integer] | None = default_percentiles,
-):
-
+def create_points_ds(points_array: npt.NDArray) -> xr.Dataset:
     # First we create a basic dataset with only dimension is point index.
     # This datset will have the data variables x, y, z and weights
     point_ds_data_vars = {
-        "x": ("idx", points["X"]),
-        "y": ("idx", points["Y"]),
-        "z": ("idx", points["Z"]),
-        "return_number": ("idx", points["ReturnNumber"]),
-        "number_of_returns": ("idx", points["NumberOfReturns"]),
+        "x": ("idx", points_array["X"]),
+        "y": ("idx", points_array["Y"]),
+        "z": ("idx", points_array["Z"]),
+        "return_number": ("idx", points_array["ReturnNumber"]),
+        "number_of_returns": ("idx", points_array["NumberOfReturns"]),
     }
 
     points_ds = xr.Dataset(data_vars=point_ds_data_vars)
+    return points_ds
 
-    if xy_bin_size is None:
-        metrics_ds = forest_z_metrics_ds(
-            points_ds, percentiles=percentiles, z_bin_size=z_bin_size
-        )
-    else:
-        x = points_ds["x"]
-        y = points_ds["y"]
-        # This algins the grid with the xy_bin_size
-        # This ensures that datasets can be merged properly
-        x_min = np.floor(x.min() / xy_bin_size) * xy_bin_size
-        y_min = np.floor(y.min() / xy_bin_size) * xy_bin_size
 
-        x_bins = np.arange(x_min, x.max() + xy_bin_size, xy_bin_size)
-        y_bins = np.arange(y_min, y.max() + xy_bin_size, xy_bin_size)
+def create_xy_grouping(points_ds: xr.Dataset, xy_bin_size: float = 1):
+    x = points_ds["x"]
+    y = points_ds["y"]
 
-        # TODO  - Make labels the center
+    # This aligns the grid with the xy_bin_size. This is not really needed
+    # but makes coordinates easier to read. Also easier to merge datasets
+    x_min = np.floor(x.min() / xy_bin_size) * xy_bin_size
+    y_min = np.floor(y.min() / xy_bin_size) * xy_bin_size
 
-        x_bin_grouper = xr.groupers.BinGrouper(
-            bins=x_bins, labels=x_bins[:-1], include_lowest=True
-        )
-        y_bin_grouper = xr.groupers.BinGrouper(
-            bins=y_bins, labels=y_bins[:-1], include_lowest=True
-        )
+    # Define bins for x and y. Essentially edges of grid cells
+    x_bins = np.arange(x_min, x.max() + xy_bin_size, xy_bin_size)
+    y_bins = np.arange(y_min, y.max() + xy_bin_size, xy_bin_size)
 
-        xy_grouping = points_ds.groupby(x=x_bin_grouper, y=y_bin_grouper)
-        metrics_ds = xy_grouping.map(
-            forest_z_metrics_ds, percentiles=percentiles, z_bin_size=z_bin_size
-        )
-        metrics_ds = metrics_ds.rename({"x_bins": "x", "y_bins": "y"})
+    # Use center of each cell as the label
+    x_labels = (x_bins[1:] + x_bins[:-1]) / 2
+    y_labels = (y_bins[1:] + y_bins[:-1]) / 2
+
+    # Define groupers for binning
+    x_bin_grouper = xr.groupers.BinGrouper(
+        bins=x_bins, labels=x_labels, include_lowest=True
+    )
+    y_bin_grouper = xr.groupers.BinGrouper(
+        bins=y_bins, labels=y_labels, include_lowest=True
+    )
+
+    return points_ds.groupby(x=x_bin_grouper, y=y_bin_grouper)
+
+
+def forest_structure_metrics(
+    points: np.ndarray,
+    xy_bin_size: float = 1,
+    z_bin_size: float = 1,
+):
+    # Turn points into a xarray dataset so we can use groupby
+    points_ds = create_points_ds(points)
+
+    # Group the dataset into x and y bins
+    xy_grouping = create_xy_grouping(points_ds, xy_bin_size=xy_bin_size)
+
+    # Calculate forest metrics for each x,y bin
+    metrics_ds = xy_grouping.map(
+        forest_z_metrics_ds,
+        z_bin_size=z_bin_size,
+        global_z_max=points_ds["z"]
+        .max()
+        .item(),  # Passing z max in makes calulating vertical profile easier
+    )
+
+    # Rename dimensions to x and y instead of x_bins and y_bins
+    metrics_ds = metrics_ds.rename({"x_bins": "x", "y_bins": "y"})
 
     metrics_ds.attrs["xy_bin_size"] = str(xy_bin_size)
     metrics_ds.attrs["z_bin_size"] = str(z_bin_size)
@@ -67,126 +82,116 @@ def forest_structure_metrics(
 
 def forest_z_metrics_ds(
     points_ds,
-    percentiles: npt.NDArray[np.integer] = None,
-    z_bin_size: float | None = 1,
+    z_bin_size: float = 1,
+    global_z_max: float | None = None,
 ):
+    grid_metrics = grid_metric_vars(points_ds)
+    voxel_metrics, voxel_z_coords = voxel_metric_vars(
+        points_ds, z_bin_size, global_z_max=global_z_max
+    )
+
+    metrics = {**grid_metrics, **voxel_metrics}
+
+    return xr.Dataset(data_vars=metrics, coords=voxel_z_coords)
+
+
+# Given a column of points to be reduced to a single value
+# Each column is a cell in the xy grid
+def grid_metric_vars(points_ds: xr.Dataset):
     z = points_ds["z"].values
     rn = points_ds["return_number"].values
     weights = (1 / points_ds["number_of_returns"]).values
 
-    metrics = {}
-    coords = {}
-
-    veg_z = z[z > 0]
-
-    metrics |= total_count_and_weight_metrics(z, rn, weights)
-    metrics |= basic_veg_point_metrics(veg_z)
-
-    if percentiles is not None and len(veg_z) > 0:
-        metrics |= veg_point_percentiles(veg_z, percentiles=percentiles)
-
-    if z_bin_size is not None:
-        m, c = z_bin_metrics(z, z_bin_size, weights=weights)
-        metrics |= m
-        coords |= c
-
-    return xr.Dataset(data_vars=metrics, coords=coords)
-
-
-def total_count_and_weight_metrics(
-    z: npt.NDArray[np.floating],
-    rn: npt.NDArray[np.floating],
-    weights: npt.NDArray[np.floating],
-):
     fr_mask = rn == 1
     veg_mask = z > 0
     ground_mask = z == 0
 
+    # Total number of points and pulses
+    num_points = len(z)
+    num_pulses = fr_mask.sum()
+
+    # Cover metrics for whole veg profile
+    total_weight = weights.sum()
+    ground_count = ground_mask.sum()
+    ground_weight = weights[ground_mask].sum()
+    veg_count = veg_mask.sum()
+    veg_weight = weights[veg_mask].sum()
+
+    lgap = ground_count / num_points if num_points > 0 else np.nan
+    lgap_weight = ground_weight / total_weight if num_points > 0 else np.nan
+
+    lcapture = 1 - lgap if num_points > 0 else np.nan
+    lcapture_weight = 1 - lgap_weight if num_points > 0 else np
+
+    # Max, mean, median height use all points
+    # 0 when highest point is ground
+    # nan when no points
+    max_height = z.max() if num_points > 0 else np.nan
+    mean_height = z.mean() if num_points > 0 else np.nan
+    median_height = np.median(z) if num_points > 0 else np.nan
+
+    crr = mean_height / max_height if max_height > 0 else np.nan
+
+    # Basic vertical complexity summary stats for veg points only
+    # e.g. not interested in standard deviation if all points are ground
+    # points (and thus have z of 0)
+    veg_z = z[veg_mask]
+    num_veg_points = len(veg_z)
+
+    sd_veg_height = veg_z.std() if num_veg_points >= 2 else np.nan
+    var_veg_height = veg_z.var() if num_veg_points >= 2 else np.nan
+    cv_veg_height = cv(veg_z) if num_veg_points >= 2 else np.nan
+    skew_veg_height = skew(veg_z) if num_veg_points >= 3 else np.nan
+    kurt_veg_height = kurtosis(veg_z) if num_veg_points >= 4 else np.nan
+    gini_veg_height = gini(veg_z) if num_veg_points >= 2 else np.nan
+
+    # Veg height for every 10th percentile
+    percentile_metrics = {
+        f"q{p}_veg_height": (np.percentile(veg_z, p) if num_veg_points > 0 else np.nan)
+        for p in np.arange(10, 100, 10)
+    }
+
     return {
-        "num_points": len(z),
-        "num_pulses": fr_mask.sum(),
-        "total_count": len(z),
-        "total_weight": weights.sum(),
-        "ground_count": ground_mask.sum(),
-        "ground_weight": weights[ground_mask].sum(),
-        "veg_count": veg_mask.sum(),
-        "veg_weight": weights[veg_mask].sum(),
+        "num_points": num_points,
+        "num_pulses": num_pulses,
+        "total_weight": total_weight,
+        "ground_count": ground_count,
+        "ground_weight": ground_weight,
+        "veg_count": veg_count,
+        "veg_weight": veg_weight,
+        "lgap": lgap,
+        "lgap_weight": lgap_weight,
+        "lcapture": lcapture,
+        "lcapture_weight": lcapture_weight,
+        "max_height": max_height,
+        "mean_height": mean_height,
+        "median_height": median_height,
+        "crr": crr,
+        "sd_veg_height": sd_veg_height,
+        "var_veg_height": var_veg_height,
+        "cv_veg_height": cv_veg_height,
+        "skew_veg_height": skew_veg_height,
+        "kurt_veg_height": kurt_veg_height,
+        "gini_veg_height": gini_veg_height,
+        **percentile_metrics,
     }
 
 
-def basic_veg_point_metrics(veg_z: npt.NDArray[np.floating]):
-
-    if len(veg_z) == 0:
-        return {
-            "max_veg": 0,
-            "mean_veg": 0,
-            "median_veg": 0,
-            "sd_veg": np.nan,
-            "var_veg": np.nan,
-            "cv_veg": np.nan,
-            "crr_veg": np.nan,
-            "skew_veg": np.nan,
-            "kurt_veg": np.nan,
-            "gini_veg": np.nan,
-        }
-
-    max_veg = veg_z.max()
-    mean_veg = veg_z.mean()
-    median_veg = np.median(veg_z)
-    sd_veg = veg_z.std()
-    var_veg = veg_z.var()
-    cv_veg = cv(veg_z)
-
-    with np.errstate(divide="ignore", invalid="ignore"):
-        crr_veg = mean_veg / max_veg
-
-    gini_veg = gini(veg_z)
-
-    if np.allclose(veg_z, veg_z[0]):
-        skew_veg = np.nan
-        kurt_veg = np.nan
-    else:
-        skew_veg = skew(veg_z)
-        kurt_veg = kurtosis(veg_z)
-
-    metrics = {
-        "max_veg": max_veg,
-        "mean_veg": mean_veg,
-        "median_veg": median_veg,
-        "sd_veg": sd_veg,
-        "var_veg": var_veg,
-        "cv_veg": cv_veg,
-        "crr_veg": crr_veg,
-        "skew_veg": skew_veg,
-        "kurt_veg": kurt_veg,
-        "gini_veg": gini_veg,
-    }
-
-    return metrics
-
-
-def veg_point_percentiles(veg_z: npt.NDArray[np.floating], percentiles: npt.ArrayLike):
-
-    percentile_metrics = {}
-    percentile_values = np.percentile(veg_z, percentiles)
-
-    for q, val in zip(percentiles, percentile_values):
-        if np.isnan(val):
-            raise ValueError("Val is nan q{q}: {val}")
-
-        percentile_metrics[f"q{q}_veg"] = val
-
-    return percentile_metrics
-
-
-def z_bin_metrics(
-    z: npt.NDArray[np.floating],
+# Given a column of points to be reduced to a single value
+# Each column is reduced to a vertical profile of voxels
+def voxel_metric_vars(
+    points_ds: xr.Dataset,
     z_bin_size: float,
-    weights: npt.NDArray[np.floating],
+    global_z_max: float | None = None,
 ):
+    z = points_ds["z"].values
+    weights = (1 / points_ds["number_of_returns"]).values
+
+    z_max = global_z_max if global_z_max is not None else z.max()
+
     # We use digitize over np.histogram because we want
     # the special case of the first bin being just 0
-    bins = np.arange(0, z.max() + z_bin_size, z_bin_size)
+    bins = np.arange(0, z_max + z_bin_size, z_bin_size)
 
     # right=True means [,0] -> 0, (0, bin_size] -> 1
     # i.e. z = 0 becomes index 0, z = 0.01 becomes index 1
@@ -198,31 +203,37 @@ def z_bin_metrics(
         bin_indices, weights=weights, minlength=len(bins)
     ).astype(float)
 
-    # Set any mising counts as nan instead of 0 - why ?
-
-    # inside[inside == 0] = np.nan
-    # inside_p = inside / total
-
-    # Index of the first non nan value of inside
-    # first_valid_value = np.argmax(~np.isnan(inside))
-    # entries = np.nancumsum(inside)
-    # entries[:first_valid_value] = np.nan
-
+    # Count the pulses that enter each voxel
     enter_count = np.nancumsum(inside_count)
     enter_weight = np.nancumsum(inside_weight)
 
+    # Count the pulses that exit each voxel
     exit_count = np.concat(([np.nan], enter_count[:-1]))
     exit_weight = np.concat(([np.nan], enter_weight[:-1]))
+
+    # Some cases enters will be 0 and the result will be NaN
+    # This is desired as the voxel has been fully occluded
+    # Separate from 0 when inside is 0 but some have passed
+    with np.errstate(divide="ignore", invalid="ignore"):
+        lgap = exit_count / enter_count
+        lgap_weight = exit_weight / enter_weight
+        # Could also be 1 - lgap
+        lcapture = inside_count / enter_count
+        lcapture_weight = inside_weight / enter_weight
 
     # Tuple metrics mean they are along
     # dimension z (i.e. 1D metrics - an array)
     metrics = {
-        "inside_count": ("z", inside_count),
-        "enter_count": ("z", enter_count),
-        "exit_count": ("z", exit_count),
-        "inside_weight": ("z", inside_weight),
-        "enter_weight": ("z", enter_weight),
-        "exit_weight": ("z", exit_weight),
+        "vox_inside_count": ("z", inside_count),
+        "vox_enter_count": ("z", enter_count),
+        "vox_exit_count": ("z", exit_count),
+        "vox_inside_weight": ("z", inside_weight),
+        "vox_enter_weight": ("z", enter_weight),
+        "vox_exit_weight": ("z", exit_weight),
+        "vox_lgap": ("z", lgap),
+        "vox_lgap_weight": ("z", lgap_weight),
+        "vox_lcapture": ("z", lcapture),
+        "vox_lcapture_weight": ("z", lcapture_weight),
     }
 
     coords = {"z": bins}
